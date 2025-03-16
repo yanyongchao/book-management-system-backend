7c501d1fbbaa9c0d55dd859ac662cf9cdc8fe899
晏永超 <1585094062@qq.com>
2025-03-16 15:02:57 +0800

    feat: first

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+    console.log(registerUserDto);
+    return 'done';
+  }
+}
